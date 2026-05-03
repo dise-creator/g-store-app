04c2ed14366c0412f5a7631dd3b90cd8ce510bfe
dise-creator <rotardanil60@gmail.com>
2026-05-03 13:57:10 +0300

    update bot banner

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -50,7 +50,7 @@ bot.start(async (ctx) => {
 
   try {
     await ctx.replyWithPhoto(
-      { url: "https://clicps.ru/banner.png" },
+      { url: "https://clicps.ru/baner.png" },
       {
         caption: [
           `🎮 *CLIC STORE* — Цифровые ключи`,
