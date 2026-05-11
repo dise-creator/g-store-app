ada8ece00fe34efc1b97f10604f1aabfa73fadb7
dise-creator <rotardanil60@gmail.com>
2026-05-11 15:02:48 +0300

    fix image optimization config

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,51 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    // Отключаем строгую оптимизацию для локальных файлов на время тестов
-    unoptimized: true, 
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: '**.cdninstagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.playstation.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.steampowered.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.steamstatic.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.t.me',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
     ],
+    formats: ['image/webp'],
+    minimumCacheTTL: 60,
   },
-  // Это заставит Next.js более агрессивно следить за папкой public
   typescript: {
     ignoreBuildErrors: true,
   },
